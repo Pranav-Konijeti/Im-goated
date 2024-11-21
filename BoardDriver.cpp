5b0730ca085ccb6ddf617ea85de8ca4909254a1d
Pranav-Konijeti <pranavkonijeti@gmail.com>
2024-11-20 20:13:13 -0700

    Update BoardDriver.cpp

--- BoardDriver.cpp
+++ BoardDriver.cpp
@@ -1,1 +1,18 @@
+#include "Board.h"
 
+
+int main(){
+    srand(time(0));
+    Board board(2,1);
+    board.initializeBoard();
+    board.displayTrack(0);
+    cout << endl;
+    board.displayTrack(1);
+    cout << endl;
+    board.displayTrack(0);
+    cout << endl;
+    board.displayTrack(1);
+    cout << endl;
+
+    return 0;
+}

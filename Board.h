933f6b1139205b231242704dfc4fca36f1b70c39
Pranav-Konijeti <pranavkonijeti@gmail.com>
2024-11-20 20:13:02 -0700

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,1 +1,28 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include "Tile.h"
 
+class Board
+{
+private:
+    static const int _BOARD_SIZE = 52;
+    Tile _tiles[2][_BOARD_SIZE];
+    static const int _MAX_PLAYERS = 2;
+    int _player_count;
+    bool _board_type;
+    int _player_position[_MAX_PLAYERS];
+    void displayTile(int player_index, int pos);
+    void initializeTiles(bool board_type);
+    bool isPlayerOnTile(int player_index, int pos);
+
+public:
+    Board();
+    Board(int player_count, bool board_type);
+    void displayTrack(int player_index);
+    void initializeBoard();
+    void displayBoard();
+    bool movePlayer(int player_index);
+    int getPlayerPosition(int player_index) const;
+};
+
+#endif

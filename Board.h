934e70b0b5ce73614eb6ffeeef569c84a141e477
Pranav-Konijeti <pranavkonijeti@gmail.com>
2024-12-09 21:59:32 -0700

    Update Board.h

--- Board.h
+++ Board.h
@@ -7,20 +7,21 @@ class Board
 {
 private:
     static const int _BOARD_SIZE = 52;
-    Tile _tiles[2][_BOARD_SIZE];
-    static const int _MAX_PLAYERS = 2;
+    Tile _tiles[5][_BOARD_SIZE];
+    static const int _MAX_PLAYERS = 5;
     int _player_count;
-    vector<bool> _board_type;
+    vector<int> _pride;
+    vector<int> _cub;
     int _player_position[_MAX_PLAYERS];
-    void displayTile(int player_index, int pos, bool board_type);
-    void initializeCubTraining(int player_index);
-    void initializePrideLands(int player_index);
+    void displayTile(vector<int> player_indexs, int pos, bool board_type);
+    void initializeCubTraining(vector<int> cub);
+    void initializePrideLands(vector<int> pride);
     bool isPlayerOnTile(int player_index, int pos);
 
 public:
     Board();
     Board(int player_count, vector<bool> board_type);
-    void displayTrack(int player_index, bool board_type);
+    void displayTrack(vector<int> player_indexs, bool board_type);
     void initializeBoard(int player_count, vector<bool> board_type);
     void displayBoard(vector<bool> board_type);
     bool movePlayer(int player_index, bool board_type);

7944b3e7da59d1892dc95f6ee073e6f1a77c298e
Eddie Chiu <84292294+lunfi@users.noreply.github.com>
2024-12-10 18:48:29 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,74 +1,129 @@
-#include <iostream>
-#include <string>
-#include "Library.h"
 #include "Player.h"
-#include "Board.h"
 
-int Library::split(string input_string, char separator, string arr[], const int ARR_SIZE){
-    int length = input_string.length();
-    int start = 0;
-    int size = 0;
-    int splits = 1;
-
-    if(input_string.empty()){
-        return 0;
-    }
-
-    for(int i = 0; i < length; i++){
-        if(input_string[i] == separator){
-            if(size < ARR_SIZE){
-                arr[size] = input_string.substr(start, i - start);
-                start = i + 1;
-                splits++;
-                size++;
-            }
-        }
-    }
-
-    if(size < ARR_SIZE){
-        arr[size] = input_string.substr(start, length - start);
-    }
-    
-    if(splits > ARR_SIZE){
-        return -1;
-    }
-    return splits;
+//Constructors
+Player::Player(){
+    _name = "";
+    _age = 1;
+    _stamina = 100;
+    _strength = 100;
+    _wisdom = 100;
 }
 
-int Library::runSpinner(int player_index){
-    srand(time(0));
-    int rNum = rand() % 6 + 1;
-    return rNum;
-}
+Player::Player(string name, int strength, int stamina, int wisdom){
+    _name = name;
 
-void Library::printMenu(){
-    cout << endl << "Main Menu" << endl;
-    cout << "Select an option to continue" << endl << endl;
-    cout << "1. Check Player Progress: Review Pride Point and Leadership Trait stats." << endl;
-    cout << "2. Review Character: Check your character name and age." << endl;
-    cout << "3. Check Position: Display board to view current position." << endl;
-    cout << "4. Review your Advisor: Check who your current advisor is on the game." << endl;
-    cout << "5. Move Forward: For each player's turn, access this option to spin the virtual spinner." << endl;
-    cout << endl;
-}
+    if(strength < 100 || strength > 1000){
+        _strength = 100;
+    }else{
+        _strength = strength;
+    }
 
-int Library::runMenu(int choice){
-    choice = 0;
+    if(stamina < 100 || stamina > 1000){
+        _stamina = 100;
+    }else{
+        _stamina = stamina;
+    }
 
-    while(true){
-        printMenu();
-        cin >> choice;
-
-        if(cin.fail()){
-            cin.clear();
-            
-        }
-
-        if(choice > 5 || choice < 1){
-            cout << "Invalid input. Please select a valid option." << endl;
-        }else{
-            return choice;
-        }
+    if(wisdom < 100 || wisdom > 1000){
+        _wisdom = 100;
+    }else{
+        _wisdom = wisdom;
     }
 }
 
+//Getters
+string Player::getName(){
+    return _name;
+}
+
+int Player::getStrength(){
+    return _strength;
+}
+
+int Player::getStamina(){
+    return _stamina;
+}
+
+int Player::getWisdom(){
+    return _wisdom;
+}
+
+int Player::getPridePoints(){
+    return _pride_points;
+}
+
+int Player::getAge(){
+    return _age;
+}
+
+//Setters
+void Player::setName(string name){
+    _name = name;
+}
+
+void Player::setStrength(int strength){
+    _strength = strength;
+}
+
+void Player::setStamina(int stamina){
+    _stamina = stamina;
+
+}
+
+void Player::setWisdom(int wisdom){
+    _wisdom = wisdom;
+
+}
+
+void Player::setPridePoints(int pride_points){
+    _pride_points = pride_points;
+}
+
+void Player::setAge(int age){
+    _age = age;
+}
+
+void Player::addStrength(int strength){
+    _strength += strength;
+}
+
+void Player::addStamina(int stamina){
+    _stamina += stamina;
+
+}
+
+void Player::addWisdom(int wisdom){
+    _wisdom += wisdom;
+
+}
+
+void Player::addPridePoints(int pride_points){
+    _pride_points += pride_points;
+}
+
+
+//Other
+void Player::toCubTraining(int strength, int stamina, int wisdom){
+    _pride_points -= 5000;
+    _strength += strength;
+    _stamina += stamina;
+    _wisdom += wisdom;
+}
+
+void Player::toPrideLands(int strength, int stamina, int wisdom){
+    _pride_points += 5000;
+    _strength += strength;
+    _wisdom += stamina;
+    _stamina += wisdom;
+}
+
+void Player::printStats(){
+    cout << "~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*" << endl;
+    cout << "Player Stats:" << endl << endl;
+    cout << _name << ", age " << _age << endl;
+    cout << "Strength: " << _strength << endl;
+    cout << "Stamina: " << _stamina << endl;
+    cout << "Wisdom: " << _wisdom << endl;
+    cout << "Pride Points: " << _pride_points << endl;
+    cout << "~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*" << endl;
+}

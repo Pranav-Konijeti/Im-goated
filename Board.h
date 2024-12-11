58b45322fec452735789d1c1e46e0a393d6a4959
Pranav-Konijeti <pranavkonijeti@gmail.com>
2024-12-11 01:19:26 -0700

    Update Board.h

--- Board.h
+++ Board.h
@@ -2,6 +2,7 @@
 #define BOARD_H
 #include "Tile.h"
 #include "Player.h"
+#include "RandomEvents.h"
 #include <vector>
 
 class Board
@@ -13,6 +14,7 @@ private:
     int _player_count;
     vector<int> _pride;
     int _player_position[_MAX_PLAYERS];
+    int _hyenaTile = 0;
     void displayTile(vector<int> player_indexs, int pos, bool board_type);
     void initializeCubTraining(vector<int> cub);
     void initializePrideLands(vector<int> pride);
@@ -24,9 +26,10 @@ public:
     void displayTrack(vector<int> player_indexs, bool board_type);
     void initializeBoard(int player_count, vector<bool> board_type);
     void displayBoard(vector<bool> board_type);
-    bool movePlayer(int player_index, bool board_type, vector<Player> playerData);
-    void playerEvent(int player_index, bool board_type, vector<Player> playerData);
+    bool movePlayer(int player_index, bool board_type, vector<Player> playerData, RandomEvents randomEvents[8], vector<int> chosenAdvisor, RandomEvents riddles[3]);
+    void playerEvent(int player_index, bool board_type, vector<Player> playerData, RandomEvents randomEvents[8], vector<int> chosenAdvisor, RandomEvents riddles[3]);
     int getPlayerPosition(int player_index) const;
+    int evaluatePlayer(int player_index, vector<Player> playerData);
 };
 
 #endif

93af853a00d8063eaec7fe41561d6493f3c0f235
Pranav-Konijeti <pranavkonijeti@gmail.com>
2024-12-10 17:18:15 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -35,3 +35,51 @@ public:
 };
 
 #endif
+
+
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Player{
+private:
+    string _name;
+    int _strength, _stamina, _wisdom, _pride_points, _age;
+
+public:
+    //Constructors
+    Player();
+    Player(string name, int strength, int stamina, int wisdom);
+
+    //Getters
+    string getName();
+    int getStrength();
+    int getStamina();
+    int getWisdom();
+    int getPridePoints();
+    int getAge();
+
+    //Setters
+    void setName(string name);
+    void setStrength(int strength);
+    void setStamina(int stamina);
+    void setWisdom(int wisdom);
+    void setPridePoints(int pride_points);
+    void setAge(int age);
+
+
+    //Adders
+    void addStrength(int strength);
+    void addStamina(int stamina);
+    void addWisdom(int wisdom);
+    void addPridePoints(int pride_points);
+
+    //Other
+    void trainCub(int strength, int stamina, int wisdom);
+    void toPrideLands(int strength, int stamina, int wisdom);
+    void printStats();
+};
+
+#endif

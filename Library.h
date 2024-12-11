ba90911a4b739512fb4213124adf4524521c4106
Eddie Chiu <84292294+lunfi@users.noreply.github.com>
2024-12-10 18:48:41 -0700

    Update Library.h

--- Library.h
+++ Library.h
@@ -8,6 +8,8 @@ struct Library
 {
     int split(string input_string, char separator, string arr[], const int ARR_SIZE);
     int runSpinner(int player_index);
+    void printMenu();
+    int runMenu(int choice);
 };
 
 #endif

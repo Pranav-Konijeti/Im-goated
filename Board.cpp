971e3d0421b3ea19e42820b6500f2d9105d71f3e
Pranav-Konijeti <pranavkonijeti@gmail.com>
2024-12-11 22:40:30 -0700

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,4 +1,4 @@
-// Worked with Eddie Chiu
+// Worked with Pranav Konijeti
 
 #include "Board.h"
 #include "Player.h"
@@ -14,9 +14,12 @@
 #define GREY "\033[48;2;128;128;128m" /* Grey (128,128,128) */
 #define RESET "\033[0m"
 
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
 
 
-void Board::initializeBoard(int player_count, vector<bool> board_type)
+
+void Board::initializeBoard(int player_count, vector<bool> board_type) //initilaizes board based on amount of players and their choice of board type
 {
     // Seed random number generator in your main function once
 
@@ -36,10 +39,7 @@ void Board::initializeBoard(int player_count, vector<bool> board_type)
     initializeCubTraining(cub);
 }
 
-#include <cstdlib> // For rand() and srand()
-#include <ctime>   // For time()
-
-void Board::initializePrideLands(vector<int> pride)
+void Board::initializePrideLands(vector<int> pride) // initilazies pride lands based on the amount of players who chose the pride lands
 {
     Tile temp;
     int green_count = 0;
@@ -54,7 +54,7 @@ void Board::initializePrideLands(vector<int> pride)
             temp.color = 'O';
         } 
         else if (i == 0) {
-            // Set the last tile as Orange for "Pride Rock"
+            // Set the first tile as the starting tile
             temp.color = 'Y';
         } 
         else if (green_count < 20 && (rand() % (total_tiles - i) < 20 - green_count)) {
@@ -97,7 +97,7 @@ void Board::initializePrideLands(vector<int> pride)
     }
 }
 
-void Board::initializeCubTraining(vector<int> cub)
+void Board::initializeCubTraining(vector<int> cub) //initalizes cub training path based on number of players who chose cub training
 {
     Tile temp;
     int green_count = 0;
@@ -156,7 +156,7 @@ void Board::initializeCubTraining(vector<int> cub)
 }
 
 
-Board::Board()
+Board::Board() // board constructor
 {
     _player_count = 1;
     _pride = {0};
@@ -168,19 +168,16 @@ Board::Board()
     initializePrideLands(_pride);
 }
 
-Board::Board(int player_count, vector<bool> board_type)
+Board::Board(int player_count, vector<bool> board_type) // board constructor initalizes board
 {
-    if (player_count > _MAX_PLAYERS)
+    if (player_count > _MAX_PLAYERS-1)
     {
-        _player_count = _MAX_PLAYERS;
+        _player_count = _MAX_PLAYERS-1;
     }
     else
     {
         _player_count = player_count;
     }
-
-    
-
 
     // Initialize player position
     for (int i = 0; i <= _player_count; i++)
@@ -193,7 +190,7 @@ Board::Board(int player_count, vector<bool> board_type)
     initializeBoard(_player_count, board_type);
 }
 
-bool Board::isPlayerOnTile(int player_index, int pos)
+bool Board::isPlayerOnTile(int player_index, int pos) // checks if player is on tile using their position index
 {
     if(_player_position[player_index] > 51){
         _player_position[player_index] = 51;
@@ -205,25 +202,23 @@ bool Board::isPlayerOnTile(int player_index, int pos)
     return false;
 }
 
-void Board::displayTile(vector<int> player_indexs, int pos, bool board_type)
+void Board::displayTile(vector<int> player_indexs, int pos, bool board_type) // displays tile, utilized in the display trakc funciton
 {
     int playerIndexSize = player_indexs.size();
-    // string space = "                                       ";
     string color = "";
     vector<bool> players;
     vector<int> onPos;
     int temp;
-    for(int i = 0; i < playerIndexSize; i++){
+    for(int i = 0; i < playerIndexSize; i++){  // this checks if each player is on the tile
         temp = isPlayerOnTile(player_indexs[i], pos);
         players.push_back(temp);
     }
-    for(int i = 0; i < playerIndexSize; i++){
+    for(int i = 0; i < playerIndexSize; i++){ // this pushes the players that are on the tiles
         if(players[i] == 1){
             onPos.push_back(player_indexs[i]);
         }
     }
-    int onPosSize = onPos.size();
-    // Template for displaying a tile: <line filler space> <color start> |<player symbol or blank space>| <reset color> <line filler space> <endl>
+    int onPosSize = onPos.size(); 
 
     // Determine color to display
     if (_tiles[board_type][pos].color == 'R')
@@ -259,12 +254,12 @@ void Board::displayTile(vector<int> player_indexs, int pos, bool board_type)
         color = GREY;
     }
 
-    if(onPosSize > 0){
+    if(onPosSize > 0){ 
         if(onPosSize == 1){
-            cout << color << "|" << (onPos[0]) << "|" << RESET;
+            cout << color << "|" << (onPos[0]) << "|" << RESET; // this displays the player if there is only one player on the tile
         }
         else{
-            cout << color << "|";
+            cout << color << "|"; // this is the display for multiple players on the tile
             for(int i = 0; i < onPosSize; i++){
                 cout << onPos[i];
                 if(i < onPosSize-1){
@@ -275,12 +270,12 @@ void Board::displayTile(vector<int> player_indexs, int pos, bool board_type)
         }
         
     }
-    else{
+    else{ // this is if there are no players on the tile
         cout << color << "| |" << RESET;
     }
 }
 
-void Board::displayTrack(vector<int> player_indexs, bool board_type)
+void Board::displayTrack(vector<int> player_indexs, bool board_type) // displays every tile in the track
 {
     for (int i = 0; i < _BOARD_SIZE; i++)
     {
@@ -289,7 +284,7 @@ void Board::displayTrack(vector<int> player_indexs, bool board_type)
     cout << endl;
 }
 
-void Board::displayBoard(vector<bool> board_type)
+void Board::displayBoard(vector<bool> board_type) // displays the two tracks as one board
 {
     int boardTypeSize = board_type.size();
     vector<int> pride;
@@ -309,7 +304,7 @@ void Board::displayBoard(vector<bool> board_type)
 }
 
 
-bool Board::movePlayer(int player_index, bool board_type, Player playerData[5], RandomEvents randomEvents[8], int chosenAdvisor[6], RandomEvents riddles[3])
+bool Board::movePlayer(int player_index, bool board_type, Player playerData[5], RandomEvents randomEvents[8], int chosenAdvisor[6], RandomEvents riddles[3]) // this moves the player and also handles any events that happens through the interntalized player event file
 {
     playerData[player_index].setTilePosition(0);
     // Increment player position
@@ -321,15 +316,18 @@ bool Board::movePlayer(int player_index, bool board_type, Player playerData[5], 
 
     _player_position[player_index+1] += moves;
     playerEvent(player_index, board_type, playerData, randomEvents, chosenAdvisor, riddles);
-    if(_hyenaTile == 1){
+    if(_hyenaTile == 1){ // checks if there on a hyena tile
         _hyenaTile = 0;
         _player_position[player_index+1] -= moves;
     }
-    if(_player_position[player_index] > 51){
+    if(_player_position[player_index+1] < 0){ // in case the players somehow go behind the track
+        _player_position[player_index+1] = 0;
+    }
+    if(_player_position[player_index] > 51){ // this makes sure hte player cant go beyond hte track
         _player_position[player_index] = 51;
         return true;
     }
-    if (_player_position[player_index] == _BOARD_SIZE - 1)
+    if (_player_position[player_index] == _BOARD_SIZE - 1) 
     {
         // Player reached last tile
         return true;
@@ -339,9 +337,9 @@ bool Board::movePlayer(int player_index, bool board_type, Player playerData[5], 
 
 void Board::playerEvent(int player_index, bool board_type, Player playerData[5], RandomEvents randomEvents[8], int chosenAdvisor[6], RandomEvents riddles[3])
 {
-    char tile = _tiles[board_type][_player_position[player_index+1]].color;
+    char tile = _tiles[board_type][_player_position[player_index+1]].color; // finds the tile the player is on and takes the corresponding character from it
     switch(tile)
-    {   case 'G':
+    {   case 'G': // case that they are on a regular grassland tile
             int greenEvent;
             greenEvent = rand() % 2;
             if(greenEvent == 0){
@@ -375,21 +373,21 @@ void Board::playerEvent(int player_index, bool board_type, Player playerData[5],
                 playerData[player_index].addPridePoints(randomEvents[actualEvent].getPridePoints());
             }
             break;
-        case 'B':
+        case 'B': // this is the oasis tile
             cout << "Player " << player_index+1 << ", you've found a peaceful oasis! This grants the player an extra turn to keep moving forward—take a deep breath and relax; you also gain 200 Stamina, Strength, and Wisdom Points." << endl << endl;
             playerData[player_index].addStamina(200);
             playerData[player_index].addStrength(200);
             playerData[player_index].addWisdom(200);
             playerData[player_index].setTilePosition(2);
             break;
-        case 'P':
+        case 'P': // this is the enrichment tile
             cout << "Player " << player_index+1 << ", welcome to the land of enrichment - when landing on this tile, your Stamina, Strength, and Wisdom Points increase by 300, and you get to choose an advisor from the available list of advisors on your next turn. If you already have an advisor, you can switch your advisor out for a different one from the list or keep your original advisor. Don’t forget - an advisor can protect you from random events that negatively impact your Pride Points." << endl << endl;
             playerData[player_index].addStamina(300);
             playerData[player_index].addStrength(300);
             playerData[player_index].addWisdom(300);
             playerData[player_index].setTilePosition(1);
             break;
-        case 'R':
+        case 'R': // this is the graveyard tile
             cout << "Player " << player_index+1 << ", uh-oh, you've stumbled into the Graveyard! This forces the player to move back 10 tiles and lose 100 Stamina, Strength, and Wisdom Points." << endl << endl;
             playerData[player_index].addStamina(-100);
             playerData[player_index].addStrength(-100);
@@ -399,12 +397,12 @@ void Board::playerEvent(int player_index, bool board_type, Player playerData[5],
                 _player_position[player_index+1] = 0;
             }
             break;
-        case 'N':
+        case 'N': // this is the hyena tile
             cout << "Player " << player_index+1 << ", the Hyenas are on the prowl! They drag you back to where you were last, and the journey comes at a cost. This returns the player to their previous position. In addition, the player's Stamina Points decrease by 300 Points." << endl << endl;
             playerData[player_index].addStamina(-300);
             _hyenaTile = 1;
             break;
-        case 'U':
+        case 'U': // this is the challenge tile
             cout << "Player " << player_index+1 << ", time for a test of wits! Land here, and you'll face a riddle randomly pulled from the riddles.txt file. Answer correctly, and you'll earn a boost of 500 Points to your Wisdom Trait—your cleverness pays off!" << endl << endl;
             int riddleEvent = rand() % 3;
             string answer;
@@ -420,7 +418,7 @@ void Board::playerEvent(int player_index, bool board_type, Player playerData[5],
     }
 }
 
-int Board::getPlayerPosition(int player_index) const
+int Board::getPlayerPosition(int player_index) const // gets player position utilizng their number stored in the player position array
 {
     if (player_index >= 0 && player_index <= _player_count)
     {
@@ -429,7 +427,7 @@ int Board::getPlayerPosition(int player_index) const
     return -1;
 }
 
-int Board::evaluatePlayer(int player_index,  Player playerData[5]){
+int Board::evaluatePlayer(int player_index,  Player playerData[5]){ // converts their leadership traits to pride points
     int points = playerData[player_index].getPridePoints() + (playerData[player_index].getStamina() * 10) + (playerData[player_index].getStrength() * 10) + (playerData[player_index].getWisdom() * 10);
     return points;
 }

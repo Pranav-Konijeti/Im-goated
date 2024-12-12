7f27648b3640eae72ac69120350017552877a9de
Eddie Chiu <84292294+lunfi@users.noreply.github.com>
2024-12-11 22:46:54 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -2,7 +2,7 @@
 
 #include "Player.h"
 
-//Constructors
+// Constructors
 Player::Player(){
     _name = "";
     _age = 1;
@@ -35,102 +35,109 @@ Player::Player(string name, int strength, int stamina, int wisdom){
     }
 }
 
-//Getters
-string Player::getName(){
+// Getters
+string Player::getName(){ // gets name of player
     return _name;
 }
 
-int Player::getStrength(){
+int Player::getStrength(){ // gets strength of player
     return _strength;
 }
 
-int Player::getStamina(){
+int Player::getStamina(){ // gets stamina of player
     return _stamina;
 }
 
-int Player::getWisdom(){
+int Player::getWisdom(){ // gets wisdom of player
     return _wisdom;
 }
 
-int Player::getPridePoints(){
+int Player::getPridePoints(){ // gets pride points of player
     return _pride_points;
 }
 
-int Player::getAge(){
+int Player::getAge(){ // gets age of player
     return _age;
 }
 
-int Player::getTilePosition(){
+int Player::getTilePosition(){ // gets position of player
     return _pos;
 }
 
 
-//Setters
-void Player::setName(string name){
+// Setters
+void Player::setName(string name){ // sets player's name
     _name = name;
 }
 
-void Player::setStrength(int strength){
+void Player::setStrength(int strength){ // sets player's strength
     _strength = strength;
 }
 
-void Player::setStamina(int stamina){
+void Player::setStamina(int stamina){ // sets player's stamina
     _stamina = stamina;
 
 }
 
-void Player::setWisdom(int wisdom){
+void Player::setWisdom(int wisdom){ // sets player's wisdom
     _wisdom = wisdom;
 
 }
 
-void Player::setPridePoints(int pride_points){
+void Player::setPridePoints(int pride_points){ // sets player's pride points
     _pride_points = pride_points;
 }
 
-void Player::setAge(int age){
+void Player::setAge(int age){ // sets player's age
     _age = age;
 }
 
-void Player::addStrength(int strength){
-    _strength += strength;
-}
-
-void Player::addStamina(int stamina){
-    _stamina += stamina;
-
-}
-
-void Player::addWisdom(int wisdom){
-    _wisdom += wisdom;
-
-}
-
-void Player::addPridePoints(int pride_points){
-    _pride_points += pride_points;
-}
-
-void Player::setTilePosition(int pos){
+void Player::setTilePosition(int pos){ // set's player's position
     _pos = pos;
 }
 
+// Adders
+void Player::addStrength(int strength){ // adds strength to player based on input
+    _strength += strength;
+    if(_strength < 100){
+        _strength = 100;
+    }
+}
 
-//Other
-void Player::toCubTraining(int strength, int stamina, int wisdom){
+void Player::addStamina(int stamina){ // adds stamina to player based on input
+    _stamina += stamina;
+    if(_stamina < 100){
+        _stamina = 100;
+    }
+}
+
+void Player::addWisdom(int wisdom){ // adds wisdom to player based on input
+    _wisdom += wisdom;
+    if(_wisdom < 100){
+        _wisdom = 100;
+    }
+}
+
+void Player::addPridePoints(int pride_points){ // adds pride points to player based on input
+    _pride_points += pride_points;
+}
+
+// Other
+void Player::toCubTraining(int strength, int stamina, int wisdom){ // cub training path
     _pride_points -= 5000;
     _strength += strength;
     _stamina += stamina;
     _wisdom += wisdom;
 }
 
-void Player::toPrideLands(int strength, int stamina, int wisdom){
+void Player::toPrideLands(int strength, int stamina, int wisdom){ // straight to pridelands path
     _pride_points += 5000;
     _strength += strength;
     _wisdom += stamina;
     _stamina += wisdom;
 }
 
-void Player::printStats(){
+void Player::printStats(){ // prints player stats
     cout << "~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*" << endl;
     cout << "Player Stats:" << endl << endl;
     cout << _name << ", age " << _age << endl;
